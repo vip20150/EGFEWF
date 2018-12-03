12110aee9379b95f910e5daf4c715950ab3713b6
vip20150 <45120677+vip20150@users.noreply.github.com>
2018-12-03 10:31:47 +0300

    Create aplayer.js

---
+++ aplayer.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'Mud Blood',
+        artist: 'Loïc Nottet',
+        url: 'http://live.mp3quran.net:9992/;',
+        cover: 'https://1.bp.blogspot.com/-ZDrDf-Vbifo/W7Ogv592LUI/AAAAAAAAGo0/SYN-rLfhHL8jYbc64edXBbnwrCqU1qn6QCLcBGAs/s1600/%25D8%25A8.png'
+    }]
+});
